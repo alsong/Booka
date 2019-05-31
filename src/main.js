bc79c3635bf61bf9c9e473f80b8bdf910727b302
Alsong <alsongdunstan2@gmail.com>
2019-05-31 03:18:26 +0300

    some changes

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Root extends React.Component{
+    renderApp(){
+        const initialState = window.__INITIAL_STATE__;
+        const store = createStore(initialState);
+
+        return(
+
+        );
+
+    }   
+
+    render(){
+        return this.renderApp();
+    }
+}
